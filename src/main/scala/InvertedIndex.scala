be1782da15fbecf7b9849c58f8f358590543d143
matiasjlopez <matias.lopez@ing.austral.edu.ar>
2017-11-24 00:18:25 -0300

    punto c) del tp, se hizo las urls mas visitadas y los request por hora

--- src/main/scala/InvertedIndex.scala
+++ src/main/scala/InvertedIndex.scala
@@ -4,16 +4,24 @@ object InvertedIndex {
   def main(args: Array[String]): Unit = {
     val sc = new SparkContext("local[*]", "InvertedIndexApp")
 
+    // Se usa wholeTextFiles para obtener los nombres de los files para poder guardarlos
     val textFile = sc.wholeTextFiles("C:/Users/Mati/Projects/Faculty/Sistemas Distribuidos/Implementation/tpspark/src/main/scala/data/books/*")
 
     textFile.flatMap{
       case (name, content) =>
-        val words = content.toLowerCase.split("""\W+""")
-        words.map(word => (word,Set(name):Set[String]))
+        val words = content.toLowerCase.split("\\W+")
+        words.map(word => {
+          val pathArray: Array[String] = name.split("/")
+          (word,Set(pathArray(pathArray.length-1)):Set[String])
+        })
     }.reduceByKey{
       case (list1, list2) =>
         list1 ++ list2
-    }.saveAsTextFile("C:/Users/Mati/Projects/Faculty/Sistemas Distribuidos/Implementation/tpspark/src/main/scala/output")
+    }.foreach{
+      case (word, files) =>
+
+        println(word + " -> " + files)
+    }
 
     sc.stop()
   }
--- src/main/scala/WordsFrequencyApp.scala
+++ src/main/scala/WordsFrequencyApp.scala
@@ -4,15 +4,16 @@ object WordsFrequencyApp {
   def main(args: Array[String]): Unit = {
     val sc = new SparkContext("local", "WordsFrequencyApp")
 
+    // RDD con las lineas de todos los archivos
     val textFile = sc.textFile("C:/Users/Mati/Projects/Faculty/Sistemas Distribuidos/Implementation/tpspark/src/main/scala/data/books/*")
 
-    textFile.map(_.toLowerCase)
-      .flatMap(line => line.split("\\W+"))
-      .map(word => (word, 1))
-      .reduceByKey(_ + _)
-      .map(_.swap)
-      .top(50)
-      .map(_.swap)
+    textFile.map(_.toLowerCase) // Se pone en minuscula a cada linea
+      .flatMap(line => line.split("\\W+")) // Se divide cada linea por palabras y como es flatmap(hace un flatten) te queda un RDD de palabras
+      .map(word => (word, 1)) // Te queda un RDD de tuplas siendo (palabra, 1)
+      .reduceByKey(_ + _) // Se aplica un reduce para que quede un RDD de (palabra, frecuencia)
+      .map(_.swap) // Cambia las tuplas para sortear por key quedando (frecuencia, palabra)
+      .top(50) // Trae los primeros 50
+      .map(_.swap) // Se vuelve a (palabra, frecuencia) para mostrar
       .foreach(println(_))
 
     sc.stop()
